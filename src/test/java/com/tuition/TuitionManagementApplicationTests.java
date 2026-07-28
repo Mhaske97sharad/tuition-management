90fb6957f7d0a1f78a3e6247ff1b8fd072736de3
Sharad Mhaske <mhaskesharad97@gmail.com>
2026-07-28 23:42:20 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/tuition/TuitionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuitionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
